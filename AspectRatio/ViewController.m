3a091629fc3d3f21c80a7e0cbcad5e243909f069
ranjith <ranjit@ranjiths-MacBook-Pro.local>
2015-10-07 14:26:23 +0530

    initial commit

--- AspectRatio/ViewController.m
+++ AspectRatio/ViewController.m
@@ -8,7 +8,13 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#import "PHPageScrollView.h"
+
+@interface ViewController ()<PHPageScrollViewDataSource, PHPageScrollViewDelegate>
+@property (weak, nonatomic) IBOutlet PHPageScrollView *pageScrollView;
+
+@property (nonatomic,strong) NSArray *dataSource;
+@property (weak, nonatomic) IBOutlet UIPageControl *pageControl;
 
 @end
 
@@ -16,7 +22,50 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.pageScrollView.delegate = self;
+    self.pageScrollView.dataSource = self;
+    [self loadBundleImages];
+}
+
+- (void)viewDidLayoutSubviews {
+    [self.pageScrollView reloadData];
+}
+
+#pragma mark - PageScrollView delegates
+
+- (NSInteger)numberOfPageInPageScrollView:(PHPageScrollView*)pageScrollView {
+    return self.dataSource.count;
+}
+
+- (CGSize)sizeCellForPageScrollView:(PHPageScrollView*)pageScrollView {
+    return CGSizeMake(self.pageScrollView.bounds.size.width, (self.pageScrollView.bounds.size.width*9)/16);
+}
+
+- (UIView*)pageScrollView:(PHPageScrollView*)pageScrollView viewForRowAtIndex:(int)index {
+    UIImageView *imageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, self.pageScrollView.bounds.size.width, self.pageScrollView.bounds.size.height)];
+    imageView.layer.shadowColor = [UIColor blackColor].CGColor;
+    imageView.layer.shadowOffset = CGSizeMake(1,1);
+    imageView.layer.shadowOpacity = 0.3;
+    imageView.image = [self.dataSource objectAtIndex:index];
+    return imageView;
+}
+
+- (void)pageScrollView:(PHPageScrollView*)pageScrollView didScrollToPageAtIndex:(NSInteger)index {
+    self.pageControl.currentPage=index;
+}
+
+- (void)pageScrollView:(PHPageScrollView*)pageScrollView willScrollToPageAtIndex:(NSInteger)index {}
+
+- (void)pageScrollView:(PHPageScrollView *)pageScrollView didTapPageAtIndex:(NSInteger)index {
+    NSLog(@"%@",[self.dataSource objectAtIndex:index]);
+}
+
+- (void)loadBundleImages {
+    if(!_dataSource) {
+        UIImage *image = [UIImage imageNamed:@"Thyrocare-444x250-30kb.jpg"];
+        UIImage *image2 = [UIImage imageNamed:@"Commonfloor-444x250-30kb-2.jpg"];
+        _dataSource = [NSArray arrayWithObjects:image,image2, nil];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
